b197f286485f63d9efa29039ebd114ab35990c23
danny45694 <danieladiazop@gmail.com>
2024-11-19 23:14:40 -0800

    created pseudocode and played around with code

---
+++ TicTacToe.js
@@ -0,0 +1,30 @@
+//Starting with Pseudocode
+
+
+// Gameboard factory function
+function gameBoard () {
+    const gameBoard = [];
+    for(i = 0; i > 9; i++) {
+        const section = ()
+        gameBoard.push(section[i])
+    }
+}
+
+// Create Players
+
+
+
+// Control game logic
+
+
+// Displaycontroller
+
+
+//Game start
+    //Render gameboard, create players and display board
+
+
+// Update board with choices and declare winner or tie
+
+
+//game end. Option to reset
