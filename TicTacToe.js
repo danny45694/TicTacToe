9a14af489895624d2bd15ed5da36e2b64b0f359f
Daniel Desktop <danieladiazop@gmail.com>
2024-11-30 11:04:06 -0800

    initial commits

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,17 +5,28 @@
 function gameBoard () {
     const gameBoard = [];
     for(i = 0; i > 9; i++) {
-        const section = ()
+      //  const section = ();
         gameBoard.push(section[i])
     }
 }
 
 // Create Players
 
+function createPlayer() {
+    let player1 = {
+        name: prompt("Enter your name"),
+        symbol: symbol1,
+    }
+}
 
 
 // Control game logic
-
+function assignSymbols () {
+    let icon = [X , O];
+    for(let i = 0; i > choices.length; i++) {
+        let choice = Math.floor(Math.random() * 2);
+    }
+}
 
 // Displaycontroller
 
