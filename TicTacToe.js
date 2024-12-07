1c0a2a299beab21b6319d79e63d2ce1a0dc0f4c0
Daniel Desktop <danieladiazop@gmail.com>
2024-12-07 14:42:00 -0800

    Worked on code and logic for game

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,14 +1,7 @@
 //Starting with Pseudocode
 
 
-// Gameboard factory function
-function gameBoard () {
-    const gameBoard = [];
-    for(i = 0; i > 9; i++) {
-      //  const section = ();
-        gameBoard.push(section[i])
-    }
-}
+
 
 // Create Players
 
@@ -20,10 +13,12 @@ function createPlayer() {
         name: prompt("Enter player2 name"),
     };
     assignSymbols(player1, player2);
+    goingFirst(player1, player2);
+    console.log(player1, player2);
 }
 
 
-// Control game logic
+// Assign symbols
 function assignSymbols (player1, player2) {
     let choice = Math.floor(Math.random() * 2);
     let symbol1 = "X";
@@ -40,6 +35,38 @@ function assignSymbols (player1, player2) {
 
 // Game Logic
 
+// Gameboard factory function
+function gameBoard () {
+    const gameBoard = [];
+
+    gameBoard.forEach(section => {
+                                                                                                                                        
+    })
+    for(i = 0; i > 9; i++) {
+      //  const section = ();
+        gameBoard.push(section[i])
+    }
+}
+
+function goingFirst(player1, player2) {
+    const heads = 1;
+    const tails = 0;
+    let player1Choice = prompt("Player 1: heads or tails");
+        if(player1Choice === "heads") {
+            player1Choice = heads
+        }
+        else {
+            player1Choice = tails;
+        }
+    let coinFlip = Math.floor(Math.random() * 2);
+        if(coinFlip === 1 && player1Choice === 1) {
+            alert("player 1 goes first");
+        } 
+        else {
+            alert("player 2 goes first");
+        }
+}
+
 function gamePlay(player1, player2) {
 
 }
@@ -47,9 +74,17 @@ function gamePlay(player1, player2) {
 //Game start
     //Render gameboard, create players and display board
 
-function gameStart() {
-    let players = createPlayer(player1, player2);
-    console.log(players);
+    function gameStart() {
+        let players = createPlayer(player1, player2);
+        console.log(players);
+    }
+
+function clearBoard() {
+    const gameGrid = document.querySelectorAll('.grid-container').forEach(item => {
+        item.addEventListener('click', event => {
+            if()
+        })
+    })
 }
 
 // Update board with choices and declare winner or tie
