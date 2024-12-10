28b6aad0b61be7102f6224cb49db0a1c555bb52e
Daniel Desktop <danieladiazop@gmail.com>
2024-12-09 23:50:31 -0800

    fixed more bugs. Got grid to be responsive. Now working on implementing turn system

--- TicTacToe.js
+++ TicTacToe.js
@@ -7,19 +7,37 @@ function gameStart() {
     let gameGrid = document.querySelectorAll('.grid-item')
     let player1 = {};
     let player2 = {};
-    let moveCount1 = 0;
-    let moveCount2 = 1;
     createPlayers(player1, player2); 
     clearBoard(gameGrid);
     assignSymbols(player1, player2);
-    goingFirst(player1, player2, moveCount1, moveCount2);
-    gamePlay(gameGrid, player1, player2, moveCount1, moveCount2);
-}
+    goingFirst(player1, player2);
+    
+    // This bottom forEach function appears to have a different scope.
+    // Console.log is not logging anything which means the code after the 
+    //click does not register.
+    
+   gameGrid.forEach(item => {
+        item.addEventListener('click', (player1, player2) => {
+            gameGrid.forEach(item => {
+                    console.log(player1, player2)
+                    if(player1.moveCount > player2.moveCount) {
+                        item.innerHTML - player1.symbol;
+                        moveCount += 1;
+                    }
+                    else {
+                        item.innerHTML = player2.symbol;
+                        moveCount +=1;
+                    }
+                })
+            })
+        })
+    }
+    //gamePlay(gameGrid, player1, player2, moveCount1, moveCount2);
+
 
 function createPlayers(player1, player2) {
     player1.name = prompt("Enter player1 name");
     player2.name = prompt("Enter player2 name");
-    console.log(player1, player2);
     return {player1, player2}
 }
 
@@ -37,12 +55,15 @@ function assignSymbols (player1, player2) {
             player1.symbol = symbol2;
             player2.symbol = symbol1;
     }
+    return {player1, player2}
  }
 
 
 function goingFirst(player1, player2) {
     const heads = 1;
     const tails = 0;
+    player1.moveCount = 0;
+    player2.moveCount = 1;
     let player1Choice = prompt("Player 1: heads or tails");
     /*    if(player1Choice != "heads" || "tails") {
             player1Choice = prompt("Error! Choose heads to tails");
@@ -56,11 +77,14 @@ function goingFirst(player1, player2) {
         }
     let coinFlip = Math.floor(Math.random() * 2);
         if(coinFlip === 1 && player1Choice === 1) {
+            player1.moveCount = 0;
             alert("player 1 goes first");
         } 
         else {
+            player1.moveCount = 1;
             alert("player 2 goes first");
         }
+        return {player1, player2}
 }
 
 //Game start
@@ -82,6 +106,8 @@ const nodeList = document.querySelectorAll('.grid-item')
     })
 }
 */
+
+
 
 
 function gamePlay(player1, player2, gameGrid, moveCount1, moveCount2) {
