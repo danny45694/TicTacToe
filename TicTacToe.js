443ebfbf0bd963f94457b0b1ac21fc2be7270f56
Daniel Desktop <danieladiazop@gmail.com>
2024-12-18 19:52:58 -0800

    I cannot figure out IIFEs

--- TicTacToe.js
+++ TicTacToe.js
@@ -6,121 +6,107 @@
 function gameStart() {
     const gameGrid = document.querySelectorAll('.grid-item')
     clearBoard(gameGrid);
-    return gameGrid;
+    const getPlayers = players();
+    getPlayers.createPlayers();
+    console.log(getPlayers);
 }
 
-const players = (function() {
-    let player1 = {};
-    let player2 = {};
-    createPlayers(player1, player2); 
-    assignSymbols(player1, player2);
-    goingFirst(player1, player2);
-    return {player1, player2}; 
-})
 
-const gameGrid = (function() {
-    const gameGrid = document.querySelectorAll('.grid-item')
 
-    const gameReset = () => {
-        for (let i = 0; i < board.length; i++) {
-            board[i] = "";
-        }
-    }
-    return gameGrid
-})
+const gameGrid = document.querySelectorAll('.grid-item')
     
     // This bottom forEach function appears to have a different scope.
     // Console.log is not logging anything which means the code after the 
-    //click does not register.
+    // click does not register.
 
-function gamePlay(player1, player2, gameGrid, moveCount1, moveCount2) {
+function gamePlay() {
+    console.log(players)
     gameGrid.forEach(item => {
         item.addEventListener('click', () => {
-            
-            if(moveCount1 > moveCount2) {
+            if(players.player1.moveCount > players.player2.moveCount) {
                 item.innerHTML = player1.symbol;
-                moveCount1 += 1;
+                player1.moveCount += 1;
             }
             else {
                 item.innerHTML = player2.symbol;
-                moveCount2 += 1;
+                player2.moveCount += 1;
             }
         })
     })
-}
+};
+
+
+const players = function () {
+    let player1 = {};
+    let player2 = {};
+
+    function playerNames() {
+        player1.name = prompt("Enter player1 name");
+        player2.name = prompt("Enter player2 name");
+    }
+
+    function assignSymbols () {
+        let choice = Math.floor(Math.random() * 2);
+        let symbol1 = "X";
+        let symbol2 = "O";
+            if(choice === 1) {
+            player1.symbol = symbol1;
+            player2.symbol = symbol2;
+            }
+            else {
+                player1.symbol = symbol2;
+                player2.symbol = symbol1;
+        }
+     }
+
+     function goingFirst() {
+        const heads = 1;
+        const tails = 0;
+        player1.moveCount = 0;
+        player2.moveCount = 1;
+        let player1Choice = prompt("Player 1: heads or tails");
+        /*    if(player1Choice != "heads" || "tails") {
+                player1Choice = prompt("Error! Choose heads to tails");
+            }
+        */
+            if(player1Choice === "heads") {
+                player1Choice = heads
+            }
+            else {
+                player1Choice = tails;
+            }
+        let coinFlip = Math.floor(Math.random() * 2);
+            if(coinFlip === 1 && player1Choice === 1) {
+                player1.moveCount = 0;
+                alert("player 1 goes first");
+            } 
+            else {
+                player1.moveCount = 1;
+                alert("player 2 goes first");
+            }
+    }
+
+    function createPlayers() {
+        playerNames();
+        assignSymbols();
+        goingFirst();
+    }
+
+    return {
+        createPlayers
+    };
+};
 
 
 
-gameGrid.forEach(item => {
-    item.addEventListener('click', event => {
-        gameGrid.forEach(item => {
-                console.log(player1, player2)
-                if(player1.moveCount > player2.moveCount) {
-                    item.innerHTML - player1.symbol;
-                    player1.moveCount += 1;
-                }
-                else {
-                    item.innerHTML = player2.symbol;
-                    player2.moveCount +=1;
-                }
-            })
-        })
-    })
-    
-    //gamePlay(gameGrid, player1, player2, moveCount1, moveCount2);
 
-
-function createPlayers(player1, player2) {
-    player1.name = prompt("Enter player1 name");
-    player2.name = prompt("Enter player2 name");
-    return {player1, player2}
-}
 
 
 // Assign symbols
-function assignSymbols (player1, player2) {
-    let choice = Math.floor(Math.random() * 2);
-    let symbol1 = "X";
-    let symbol2 = "O";
-        if(choice === 1) {
-        player1.symbol = symbol1;
-        player2.symbol = symbol2;
-        }
-        else {
-            player1.symbol = symbol2;
-            player2.symbol = symbol1;
-    }
-    return {player1, player2}
- }
 
 
-function goingFirst(player1, player2) {
-    const heads = 1;
-    const tails = 0;
-    player1.moveCount = 0;
-    player2.moveCount = 1;
-    let player1Choice = prompt("Player 1: heads or tails");
-    /*    if(player1Choice != "heads" || "tails") {
-            player1Choice = prompt("Error! Choose heads to tails");
-        }
-    */
-        if(player1Choice === "heads") {
-            player1Choice = heads
-        }
-        else {
-            player1Choice = tails;
-        }
-    let coinFlip = Math.floor(Math.random() * 2);
-        if(coinFlip === 1 && player1Choice === 1) {
-            player1.moveCount = 0;
-            alert("player 1 goes first");
-        } 
-        else {
-            player1.moveCount = 1;
-            alert("player 2 goes first");
-        }
-        return {player1, player2}
-}
+
+
 
 //Game start
     //Render gameboard, create players and display boardruby
