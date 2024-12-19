d8330ff6410242d467cbb2e02e2b87d416bff223
danny45694 <danieladiazop@gmail.com>
2024-12-18 22:49:30 -0800

    Finally got grid to work and added a check for already played spaces. Now to program win conditions

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,109 +1,96 @@
-//Starting with Pseudocode
-
-
-// Create Players
+let player1 = {};
+let player2 = {};
 
 function gameStart() {
     const gameGrid = document.querySelectorAll('.grid-item')
     clearBoard(gameGrid);
-    const getPlayers = players();
-    getPlayers.createPlayers();
-    console.log(getPlayers);
+    playerNames();
+    assignSymbols();
+    goingFirst();
+
+
+    gameGrid.forEach(item => {
+        item.addEventListener('click', () => {
+            if(item.innerHTML === "X" || item.innerHTML === "O") {
+                alert("Space taken. Click on a different space")
+                return
+            }
+            if(player1.moveCount < player2.moveCount) {
+                    item.innerHTML = player1.symbol;
+                    player1.moveCount += 1;
+            }
+            else {
+                    item.innerHTML = player2.symbol;
+                    player2.moveCount += 1;
+            }
+        })
+    })
+}
+
+
+// Program Win Condition and checks against using same square
+
+function winCondition() {
+    /* If X or O are in the followings locations, you win. 
+    1,2,3
+    4,5,6 
+    7,8,9
+    1,4,7
+    2,5,8
+    3,6,9
+    1,5,9
+    3,5,7 
+    */
 }
 
 
 
-const gameGrid = document.querySelectorAll('.grid-item')
-    
-    // This bottom forEach function appears to have a different scope.
-    // Console.log is not logging anything which means the code after the 
-    // click does not register.
 
-function gamePlay() {
-    console.log(players)
-    gameGrid.forEach(item => {
-        item.addEventListener('click', () => {
-            if(players.player1.moveCount > players.player2.moveCount) {
-                item.innerHTML = player1.symbol;
-                player1.moveCount += 1;
-            }
-            else {
-                item.innerHTML = player2.symbol;
-                player2.moveCount += 1;
-            }
-        })
-    })
-};
+function playerNames() {
+    player1.name = prompt("Enter player1 name");
+    player2.name = prompt("Enter player2 name");
+}
 
-
-const players = function () {
-    let player1 = {};
-    let player2 = {};
-
-    function playerNames() {
-        player1.name = prompt("Enter player1 name");
-        player2.name = prompt("Enter player2 name");
-    }
-
-    function assignSymbols () {
-        let choice = Math.floor(Math.random() * 2);
-        let symbol1 = "X";
-        let symbol2 = "O";
-            if(choice === 1) {
-            player1.symbol = symbol1;
-            player2.symbol = symbol2;
-            }
-            else {
-                player1.symbol = symbol2;
-                player2.symbol = symbol1;
+function assignSymbols () {
+    let choice = Math.floor(Math.random() * 2);
+    let symbol1 = "X";
+    let symbol2 = "O";
+        if(choice === 1) {
+        player1.symbol = symbol1;
+        player2.symbol = symbol2;
         }
-     }
-
-     function goingFirst() {
-        const heads = 1;
-        const tails = 0;
-        player1.moveCount = 0;
-        player2.moveCount = 1;
-        let player1Choice = prompt("Player 1: heads or tails");
-        /*    if(player1Choice != "heads" || "tails") {
-                player1Choice = prompt("Error! Choose heads to tails");
-            }
-        */
-            if(player1Choice === "heads") {
-                player1Choice = heads
-            }
-            else {
-                player1Choice = tails;
-            }
-        let coinFlip = Math.floor(Math.random() * 2);
-            if(coinFlip === 1 && player1Choice === 1) {
-                player1.moveCount = 0;
-                alert("player 1 goes first");
-            } 
-            else {
-                player1.moveCount = 1;
-                alert("player 2 goes first");
-            }
+        else {
+            player1.symbol = symbol2;
+            player2.symbol = symbol1;
+    }
     }
 
-    function createPlayers() {
-        playerNames();
-        assignSymbols();
-        goingFirst();
-    }
-
-    return {
-        createPlayers
-    };
-};
-
-
-
-
-
-
-// Assign symbols
-
+function goingFirst() {
+    const heads = 1;
+    const tails = 0;
+    player1.moveCount = 0;
+    player2.moveCount = 1;
+    let player1Choice = prompt("Player 1: heads or tails");
+    /*    if(player1Choice != "heads" || "tails") {
+            player1Choice = prompt("Error! Choose heads to tails");
+        }
+    */
+        if(player1Choice === "heads") {
+            player1Choice = heads
+        }
+        else {
+            player1Choice = tails;
+        }
+    let coinFlip = Math.floor(Math.random() * 2);
+        if(coinFlip === 1 && player1Choice === 1) {
+            player1.moveCount = 0;
+            alert("player 1 goes first");
+        } 
+        else {
+            player1.moveCount = 1;
+            alert("player 2 goes first");
+        }
+}
 
 
 
@@ -117,16 +104,6 @@ function clearBoard(gameGrid) {
     })
 }
 
-
-
-/* function testing() {
-const nodeList = document.querySelectorAll('.grid-item')
-
-    nodeList.forEach(node => {
-        console.log(node);
-    })
-}
-*/
 
 
 
